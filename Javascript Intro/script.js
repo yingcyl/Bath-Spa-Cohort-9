ba67ab747a086d7ffc16b2c82759ec0265ea86b3
yingcyl <chiayinglow@gmail.com>
2022-09-27 16:06:02 +0100

    Edits

--- Javascript Intro/script.js
+++ Javascript Intro/script.js
@@ -1,11 +1,3 @@
-function turnOn() {
-    document.getElementById("bulb").src = "pic_bulbon.gif";
-}
-
-function turnOff() {
-    document.getElementById("bulb").src = "pic_bulboff.gif";
-}
-
 function changeImage() {
     document.getElementById("donut").src = "pink.jpeg";
 }
