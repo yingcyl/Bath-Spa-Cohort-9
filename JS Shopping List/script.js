fcc42169dace78e94277199f5a6729b11f85c03c
yingcyl <chiayinglow@gmail.com>
2022-09-29 17:14:15 +0100

    Comments

--- JS Shopping List/script.js
+++ JS Shopping List/script.js
@@ -8,10 +8,13 @@ let newList = [];
 button.addEventListener("click", () => {
     // all input converted to lowercase to prevent duplicates being entered but with different letter cases
     const newItem = (input.value).toLowerCase();
+    //checking if array already has the item 
     if (newList.includes(newItem)) {
         alert("Item is already on the list!")
     } else {
+        //if array doesn't have item, the item will be pushed in to the array
         newList.push(newItem);
+        //creating a new li element 
         const addList = document.createElement("li");
         // li tags appended to ul 
         list.append(addList);
